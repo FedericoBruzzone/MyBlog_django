7a48c7b930a38dc63cef1dc7f743c0a1e4a75dbb
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-07-30 01:10:26 +0200

    Added forms.py made with boostrap

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DeleteView
-
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 #def home(request): # @Dev
 #    return render(request, 'home.html', {}) # @Dev
@@ -11,6 +11,14 @@ class HomeView(ListView):
     template_name = 'home.html'
 
 
-class ArticleDetailView(DeleteView):
+class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    #fields = '__all__'
+    #fields = ('title', 'body')
+
