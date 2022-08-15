bd77c2a5f613e1352e06683106d4f6fe5727f29a
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-08-16 01:25:58 +0200

    Add like button

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Category
 from .forms import PostForm, EditForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 
 #def home(request): # @Dev
 #    return render(request, 'home.html', {}) # @Dev
@@ -28,6 +29,11 @@ def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats.replace('-', ' '))
     return render(request, 'categories.html', {'cats':cats.title().replace('-', ' '), 'category_posts':category_posts})
 
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('article_detail', args=[str(pk)]))
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
@@ -35,7 +41,12 @@ class ArticleDetailView(DetailView):
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
         context = super(DetailView, self).get_context_data(*args, **kwargs)
+
+        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+
         context["cat_menu"] = cat_menu
+        context["total_likes"] = total_likes
         return context
 
 class AddPostView(CreateView):
