14403268eaeaffd7daf57789fc4d6541293ddf5a
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-08-18 01:19:39 +0200

    Add change password form-url-view

--- ablog/members/urls.py
+++ ablog/members/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
-    # path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password.html')),
-    path('password/', PasswordsChangeView.as_view(template_name='registration/change_password.html')),
+    #path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password.html')),
+    path('password/', PasswordsChangeView.as_view()),
     path('password_success', views.password_success, name='password_success'),
 
 
--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.urls import reverse_lazy
-from .forms import SignUpForm, EditProfileForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
+from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
+
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm #UserCreationForm
@@ -19,8 +20,10 @@ class UserEditView(generic.UpdateView):
         return self.request.user
 
 class PasswordsChangeView(PasswordChangeView):
-    form_class = PasswordChangeView
-    success_url = reverse_lazy('home')
-
+    form_class = PasswordChangingForm #PasswordChangeForm
+    template_name='registration/change_password.html'
+    #success_url = reverse_lazy('home')
+    success_url = reverse_lazy('password_success')
+    
 def password_success(request):
     return render(request, 'registration/password_success.html', {})
