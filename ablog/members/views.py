0f3471fddcbbd7ef853b6a87d4cc74d0f31e5b05
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-08-30 17:18:05 +0200

    Add profile page

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from gc import get_objects
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.views.generic import DetailView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
-
+from theblog.models import Profile
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm #UserCreationForm
@@ -27,3 +29,16 @@ class PasswordsChangeView(PasswordChangeView):
     
 def password_success(request):
     return render(request, 'registration/password_success.html', {})
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        # users = Profile.objects.all()
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+        
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+        
+        context["page_user"] = page_user
+        return context
