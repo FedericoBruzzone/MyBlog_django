fbcb83d50704cc00d7bcba13fb6160e23cb37177
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-08-30 15:56:37 +0200

    Add card profile on a blog post

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -42,6 +42,16 @@ class Category(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+    website_url = models.CharField(max_length=255, blank=True)
+    linkedin_url = models.CharField(max_length=255, blank=True)
+    indeed_url = models.CharField(max_length=255, blank=True)
+    facebook_url = models.CharField(max_length=255, blank=True)
+    twitter_url = models.CharField(max_length=255, blank=True)
+    instagram_url = models.CharField(max_length=255, blank=True)
+    pinterest_url = models.CharField(max_length=255, blank=True)
+
+
 
     def __str__(self):
         return str(self.user)
