2f5e1ca1489883973c13ccdeb6792a9a73968202
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-09-01 01:20:03 +0200

    Update comment section

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -2,17 +2,18 @@ from django.urls import path
 
 #from ablog.theblog.models import Category
 #from . import views 
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView, LikeView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView, LikeView, AddCommentView
 
 urlpatterns = [
     #path('', views.home, name="home"),
     path('', HomeView.as_view(), name="home"),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
+    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
-    path('atricle/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
-    path('atricle/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
+    path('atricle/edit/<int:pk>/', UpdatePostView.as_view(), name='update_post'),
+    path('atricle/<int:pk>/remove/', DeletePostView.as_view(), name='delete_post'),
     path('add_category/', AddCategoryView.as_view(), name='add_category'),
     path('category/<str:cats>/', CategoryView, name='category'),
     path('category-list/', CategoryListView, name='category-list'),
-    path('like/<int:pk>', LikeView, name='like_post'),
+    path('like/<int:pk>/', LikeView, name='like_post'),
+    path('atricle/<int:pk>/comment/', AddCommentView.as_view(), name='add_comment'),
 ]
