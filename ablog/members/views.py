a9e3134869048745b90e29e33615924509ff7e59
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-09-01 00:27:06 +0200

    Add 'create profile page' for new users

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,11 +1,11 @@
 from gc import get_objects
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
+from .forms import SignUpForm, EditProfileForm, PasswordChangingForm, ProfilePageForm
 from theblog.models import Profile
 
 class UserRegisterView(generic.CreateView):
@@ -56,3 +56,12 @@ class EditProfilePageView(generic.UpdateView):
         'instagram_url', 
         'pinterest_url']
     success_url = reverse_lazy('home')
+
+class CreateProfilePageView(CreateView):
+    model = Profile
+    form_class = ProfilePageForm
+    template_name = "registration/create_user_profile_page.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
